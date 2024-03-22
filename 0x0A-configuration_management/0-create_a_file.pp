8eff8ffa80f1cf9ebf6d8342c7697df3247c6d12
gomolemontlhane <gomolemontlhane@gmail.com>
2024-03-22 15:58:14 -0700

    fixing pupper-lint errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Puppet manifest to create a file in /tmp
 
 file { '/tmp/school':
-  ensure   => file,
-  mode     => '0744',
-  owner    => 'www-data',
-  group    => 'www-data',
-  content  => 'I love Puppet',
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
 

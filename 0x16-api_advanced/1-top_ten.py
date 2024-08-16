3b73ecc4c71686ffedfee53084a0f54aa105347a
Gomolemo Ntlhane <gomolemontlhane@gmail.com>
2024-08-16 03:51:48 +0200

    resubmitting api files

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
+"""queries and prints titles of 10 host posts"""
 import requests
 
+
 def top_ten(subreddit):
-    """Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit."""
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'custom user agent'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code != 200:
+    """Function that queries the Reddit API"""
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
+
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
+    else:
         print(None)
-        return
-    
-    data = response.json()
-    posts = data.get('data', {}).get('children', [])
-    for post in posts[:10]:
-        print(post.get('data', {}).get('title', ''))
